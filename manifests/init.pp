5b7c8b579827b79689259ca7da73508a1b5a29da
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2019-02-05 00:25:05 +0100

    reindent
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,30 +58,31 @@
 # [Remember: No empty lines between comments and class definition]
 #
 class exim4(
-    $ensure        = $exim4::params::ensure,
-    $configtype    = $exim4::params::configtype,
-    $localdelivery = $exim4::params::localdelivery,
-    $smarthost     = $exim4::params::smarthost,
-    $nodnslookup   = $exim4::params::nodnslookup,
-    ) inherits exim4::params
-{
+  $ensure        = $exim4::params::ensure,
+  $configtype    = $exim4::params::configtype,
+  $localdelivery = $exim4::params::localdelivery,
+  $smarthost     = $exim4::params::smarthost,
+  $nodnslookup   = $exim4::params::nodnslookup,
+
+) inherits exim4::params
+  {
     info ("Configuring exim4 (with ensure = ${ensure}, configtype = ${configtype})")
 
     if ! ($ensure in [ 'present', 'absent' ]) {
-        fail("exim4 'ensure' parameter must be set to either 'absent' or 'present'")
+      fail("exim4 'ensure' parameter must be set to either 'absent' or 'present'")
     }
     if ! ($configtype in [ 'none', 'local', 'smarthost', 'satellite', 'internet' ]) {
-        fail("exim4 'configtype' parameter must be set to 'internet', 'smarthost', 'satellite', 'local', or 'none'")
+      fail("exim4 'configtype' parameter must be set to 'internet', 'smarthost', 'satellite', 'local', or 'none'")
     }
     if ($configtype in [ 'smarthost', 'internet' ]) {
-        warning("configtype ${configtype} NOT YET IMPLEMENTED for exim4")
+      warning("configtype ${configtype} NOT YET IMPLEMENTED for exim4")
     }
 
     case $::operatingsystem {
-        'debian', 'ubuntu':         { include ::exim4::debian }
-        'redhat', 'fedora', 'centos': { include ::exim4::redhat }
-        default: {
-            fail("Module ${module_name} is not supported on ${::operatingsystem}")
-        }
+      'debian', 'ubuntu':         { include ::exim4::debian }
+      'redhat', 'fedora', 'centos': { include ::exim4::redhat }
+      default: {
+        fail("Module ${module_name} is not supported on ${::operatingsystem}")
+      }
     }
-}
+  }
