1e3e9d54a15675d33c8c81cf3f4833aa3621515e
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2026-05-13 16:08:12 +0200

    refactor: lint

--- manifests/common.pp
+++ manifests/common.pp
@@ -10,49 +10,47 @@
 #
 # Note: respect the Naming standard provided here[http://projects.puppetlabs.com/projects/puppet/wiki/Module_Standards]
 class exim4::common {
+  # Load the variables used in this module. Check the exim4-params.pp file
+  require exim4::params
 
-    # Load the variables used in this module. Check the exim4-params.pp file
-    require exim4::params
+  package { 'exim4':
+    ensure => $exim4::ensure,
+    name   => $exim4::params::packagename,
+  }
 
-    package { 'exim4':
-        ensure => $exim4::ensure,
-        name   => $exim4::params::packagename,
-    }
+  service { 'exim4':
+    ensure     => running,
+    name       => $exim4::params::servicename,
+    enable     => true,
+    hasrestart => $exim4::params::hasrestart,
+    pattern    => $exim4::params::processname,
+    hasstatus  => $exim4::params::hasstatus,
+    require    => Package['exim4'],
+    subscribe  => File[$exim4::params::configfile],
+  }
 
-    service { 'exim4':
-        ensure     => running,
-        name       => $exim4::params::servicename,
-        enable     => true,
-        hasrestart => $exim4::params::hasrestart,
-        pattern    => $exim4::params::processname,
-        hasstatus  => $exim4::params::hasstatus,
-        require    => Package['exim4'],
-        subscribe  => File[$exim4::params::configfile],
-    }
+  file { $exim4::params::logdir:
+    ensure  => directory,
+    mode    => $exim4::params::logdir_mode,
+    owner   => $exim4::params::logdir_owner,
+    group   => $exim4::params::logdir_group,
+    notify  => Service['exim4'],
+    require => Package['exim4'],
+  }
 
-    file { $exim4::params::logdir:
-        ensure  => directory,
-        mode    => $exim4::params::logdir_mode,
-        owner   => $exim4::params::logdir_owner,
-        group   => $exim4::params::logdir_group,
-        notify  => Service['exim4'],
-        require => Package['exim4'],
-    }
+  file { $exim4::params::configfile:
+    ensure  => $exim4::ensure,
+    owner   => $exim4::params::configfile_owner,
+    group   => $exim4::params::configfile_group,
+    mode    => $exim4::params::configfile_mode,
+    notify  => Service['exim4'],
+    require => Package['exim4'],
+  }
 
-    file { $exim4::params::configfile:
-        ensure  => $exim4::ensure,
-        owner   => $exim4::params::configfile_owner,
-        group   => $exim4::params::configfile_group,
-        mode    => $exim4::params::configfile_mode,
-        notify  => Service['exim4'],
-        require => Package['exim4'],
-    }
-
-    # Create a ["blackhole" alias]{http://www.exim.org/exim-html-3.20/doc/html/spec_23.html#SEC634}
-    mailalias { 'blackhole':
-        ensure    => $exim4::ensure,
-        recipient => ':blackhole:',
-        require   => Package['exim4'],
-    }
-
+  # Create a ["blackhole" alias]{http://www.exim.org/exim-html-3.20/doc/html/spec_23.html#SEC634}
+  mailalias { 'blackhole':
+    ensure    => $exim4::ensure,
+    recipient => ':blackhole:',
+    require   => Package['exim4'],
+  }
 }
--- manifests/debian.pp
+++ manifests/debian.pp
@@ -8,23 +8,21 @@
 #
 # Specialization class for Debian systems
 class exim4::debian inherits exim4::common {
+  package { $exim4::params::utils_packages:
+    ensure  => $exim4::ensure,
+    require => Package['exim4'],
+  }
 
-    package { $exim4::params::utils_packages:
-        ensure  => $exim4::ensure,
-        require => Package['exim4'],
-    }
+  file { '/etc/mailname':
+    ensure  => file,
+    mode    => $exim4::params::configfile_mode,
+    owner   => $exim4::params::configfile_owner,
+    group   => $exim4::params::configfile_group,
+    require => Package['exim4'],
+    content => "${facts['networking']['fqdn']}\n",
+  }
 
-    file { '/etc/mailname':
-        ensure  => present,
-        mode    => $exim4::params::configfile_mode,
-        owner   => $exim4::params::configfile_owner,
-        group   => $exim4::params::configfile_group,
-        require => Package['exim4'],
-        content => "${facts['networking']['fqdn']}\n",
-    }
-
-    File[$exim4::params::configfile] {
-        content => template('exim4/update-exim4.conf.conf.erb')
-    }
-
+  File[$exim4::params::configfile] {
+    content => template('exim4/update-exim4.conf.conf.erb')
+  }
 }
